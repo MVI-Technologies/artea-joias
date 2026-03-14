abac68a5ff08ac00ed874f4d595f5b023f321ee5
Matheus-Imberio <matheusimberio030@gmai.com>
2026-03-14 12:57:02 -0300

    feat: Add migration file for the `get_lot_name` function.

--- supabase/migrations/058_get_lot_name_function.sql
+++ supabase/migrations/058_get_lot_name_function.sql
@@ -1,12 +1,1 @@
-CREATE OR REPLACE FUNCTION get_lot_name_by_id(p_lot_id UUID)
-RETURNS TEXT
-LANGUAGE plpgsql
-SECURITY DEFINER
-AS $$
-DECLARE
-  v_nome TEXT;
-BEGIN
-  SELECT nome INTO v_nome FROM lots WHERE id = p_lot_id;
-  RETURN v_nome;
-END;
-$$;
+ 
